6555815b373ed1fa625ac1cd41dc6e00f781542e
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2015-12-26 15:24:00 +0000

    update ebooks

--- assets/helpers/to-html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in ./src-tex_epub/*.tex
-do
-  outfile="./src-html/`basename $i`.html"
-  echo $i
-  pandoc --smart --normalize -f latex -t html -o "$outfile" "$i"
-  sed -i 's/<\/*blockquote>//g' "$outfile"
-done
